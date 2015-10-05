a475372f53ecf0eea596baf9a25a722908234522
Aaron Douglas <astralbodies@gmail.com>
2015-10-05 15:11:32 -0500

    Initial Commit

---
+++ TaterTotTimer WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  TaterTotTimer WatchKit Extension
+//
+//  Created by Aaron Douglas on 10/5/15.
+//  Copyright © 2015 Automattic. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
