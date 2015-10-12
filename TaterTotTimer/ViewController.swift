f10f62417942beecea9c1c77c97b620fa94f8278
Aaron Douglas <astralbodies@gmail.com>
2015-10-12 08:01:14 -0500

    Icon changes and getting closer to being complete

--- TaterTotTimer/ViewController.swift
+++ TaterTotTimer/ViewController.swift
@@ -9,8 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var totalNumberOfTots = 0
     @IBOutlet var totCountLabel: UILabel!
+    @IBOutlet var totImage: UIImageView!
+    @IBOutlet var startStopButton: UIButton!
+    @IBOutlet var timerFace: UILabel!
+
+    var totalNumberOfTots = 5
+    var timer: NSTimer?
+    var targetDate: NSDate?
+    var degrees = 0.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,7 +33,7 @@ class ViewController: UIViewController {
 
     
     func updateTotLabel() {
-        totCountLabel.text = "\(totalNumberOfTots)"
+        totCountLabel.text = "Number of Tots: \(totalNumberOfTots)"
     }
     
     @IBAction func changeTotValue(sender: UIStepper) {
@@ -34,6 +41,51 @@ class ViewController: UIViewController {
         updateTotLabel()
     }
     
+    @IBAction func startOrStopTimer() {
+        if let timer = timer {
+            timer.invalidate()
+            self.timer = nil
+            targetDate = nil
+            totImage.transform = CGAffineTransformIdentity
+            degrees = 0.0
+            startStopButton.setTitle("Start Timer", forState: .Normal)
+            timerFace.hidden = true
+            return
+        }
+        
+        let dateComponents = NSDateComponents.init()
+        let calendar = NSCalendar.currentCalendar()
+        dateComponents.second = timeForNumberOfTots(totalNumberOfTots)
+        targetDate = calendar.dateByAddingComponents(dateComponents, toDate: NSDate.init(), options: [])
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "updateTot", userInfo: nil, repeats: true)
+        startStopButton.setTitle("Stop Timer", forState: .Normal)
+        timerFace.hidden = false
+        updateTot()
+    }
+    
+    func updateTot() {
+        degrees += 20
+        totImage.transform = CGAffineTransformMakeRotation(CGFloat(degrees * M_PI/180));
+        
+        let calendar = NSCalendar.currentCalendar()
+        let dateComponents = calendar.components([.Minute, .Second], fromDate: NSDate.init(), toDate: targetDate!, options: [])
+        timerFace.text = "\(dateComponents.minute):\(dateComponents.second)"
+    }
+    
+    func timeForNumberOfTots(numberOfTots:Int) -> Int {
+        if (numberOfTots > 0 && numberOfTots <= 20) {
+            return 22 * 60;
+        } else if (numberOfTots <= 30) {
+            return 24 * 60;
+        } else {
+            return 26 * 60;
+        }
+    }
+    
+    func scheduleLocalNotification(targetDate: NSDate) {
+        
+    }
 
 }
 
